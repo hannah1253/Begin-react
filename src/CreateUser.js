0bc7e24c8f5767f9b8fd88f8bf4b89393cedf869
hannah <hyunah1253@naver.com>
2025-07-21 13:36:03 +0900

    test

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,23 +1,23 @@
 import React from "react";
 
-function CreateUesr({username, email,onChange,onCreate}){
-    return(
-        <div>
-            <input 
-                name="username" 
-                placeholder="계정명" 
-                onChange={onChange} 
-                value={username}
-            />
-            <input 
-                name="email" 
-                placeholder="이메일" 
-                onChange={onChange} 
-                value={email}
-            />
-        <button onClick={onCreate}>등록</button>
-        </div>
-    )
+function CreateUesr({ username, email, onChange, onCreate }) {
+  return (
+    <div>
+      <input
+        name="username"
+        placeholder="계정명"
+        onChange={onChange}
+        value={username}
+      />
+      <input
+        name="email"
+        placeholder="이메일"
+        onChange={onChange}
+        value={email}
+      />
+      <button onClick={onCreate}>등록</button>
+    </div>
+  );
 }
 
 export default React.memo(CreateUesr);
